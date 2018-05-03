d3fc1bca2958ed7cccbbe46b32e9dadec50ba407
VITOLDO <viktorfilonenko@gmail.com>
2018-05-03 17:22:08 +0300

    donation + dynamic attendants

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,16 +4,70 @@ contract Splitter {
 	
 	address public owner;
 
-	function Splitter() public {
+	uint public funds;
+
+	struct SplitterAttendant {
+    	address account;
+    	uint donated;
+    	uint balance;
+  	}
+
+  	SplitterAttendant[] public attendants;
+
+	function Splitter(address _aliceAdd, 
+					  address _bobAdd, 
+					  address _carolAdd) 
+		public 	
+	{
 		owner = msg.sender;
+		attendants.push(SplitterAttendant(_aliceAdd, 0, 0));
+		attendants.push(SplitterAttendant(_bobAdd, 0, 0));
+		attendants.push(SplitterAttendant(_carolAdd, 0, 0));
 	}
 
-	function readBalance(address) public constant returns(uint balance) {
-		return 5;
-	}
-
-	function giveSplittedFund() public payable returns(bool success){
+	function donateFund() 
+		public 
+		payable 
+		returns(bool success)
+	{
 		require(msg.value > 0);
+
+		funds += msg.value;
+
+		uint numberOfAttendats = attendants.length;
+		for(uint i = 0; i < numberOfAttendats; i++) {
+			findAttendant(msg.sender).donated += msg.value;
+		}
 		return true;
+	}
+
+	function readAttendantsCount()
+		public
+		constant
+		returns(uint count)
+	{
+		return attendants.length;
+	}
+
+	function readAttendantDonation(address attendant)
+		public
+		constant
+		returns(uint count)
+	{
+		return findAttendant(attendant).donated;
+	}
+
+	function findAttendant(address attendant)
+		internal
+		constant
+		returns(SplitterAttendant resultAttendant)
+	{
+		uint numberOfAttendats = attendants.length;
+		for(uint i = 0; i < numberOfAttendats; i++) {
+			SplitterAttendant memory iteratedAttendant = attendants[i];
+			if (attendant == iteratedAttendant.account) {
+				return iteratedAttendant;
+			}
+		}
 	}
 }
--- test/metacoin.js
+++ test/metacoin.js
@@ -3,16 +3,51 @@ var Splitter = artifacts.require("./Splitter.sol");
 contract('Splitter', function(accounts) {
 
   var contract;
+  var aliceAddress = accounts[1];
+  var bobAddress = accounts[2];
+  var carolAddress = accounts[3];
+
+  var owner = accounts[0]
 
   beforeEach(function() {
-    return Splitter.new({from: accounts[0]})
+    return Splitter.new(aliceAddress, bobAddress, carolAddress, {from: owner})
     .then(function(instance) {
       contract = instance;
-    })
-  })
+    });
+  });
 
   it("should just say hello", function() {
       assert.equal(true, true, "Boolean logick isn't working!!!");
   });
   
+  it("contract should be owned by owner", function() {
+    return contract.owner({from: owner})
+    .then(function(_owner){
+      assert.strictEqual(_owner, owner, "Contract is not owned by owner");
+    });
+  }); 
+
+  it("contract should contains exact 3 attendats", function() {
+    return contract.readAttendantsCount({from: owner})
+    .then(function(count){
+      assert.equal(3, count, "There are some new/lost attendants in attendants list.");
+    });
+  });
+
+  it("should be possible to donate", function() {
+    return contract.donateFund({from: aliceAddress, value: 1})
+    .then(function(txHash){
+      return contract.funds({from: owner})
+      .then(function(funds){
+        assert.strictEqual('1', funds.toString(10), "Donation was not processed.");
+      });
+      return contract.readAttendantDonation(aliceAddress, {from: owner})
+      .then(function(donation) {
+        assert.strictEqual('1', donation.toString(10), "Donation was not equal to previously donated value or was missing.");
+      });
+    });
+  });
+
+  
+
 });
